5846aec83dd84eecb610ab7c79a96a851043fb7d
jeking123 <azunaki1@gmail.com>
2016-11-22 14:34:02 -0800

    Project 3 Set up
    Set Up
    Random image
    Light Box

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-// My Scripts
+// Randomize Header
+
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
+
+// Store images
+var img0 = 'img/fullsize/1.png';
+var img1 = 'img/fullsize/2.jpg';
+var img2 = 'img/fullsize/3.png';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
+
+// Activate Gallery
+
+$('figure').lightGallery({
+    selector: 'a'
+});
